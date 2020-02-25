1f51f191cfa573e4f26d227d3e150f2a8c90efe6
Scott Schwartz <Scott.Schwartz@xaxis.com>
2020-02-25 08:40:49 -0500

    day 3 part 2

--- 3/aoc2019-3.js
+++ 3/aoc2019-3.js
@@ -1,15 +1,15 @@
-const { each, head, tail, join, toNumber, intersectionBy, toString, min, sum, map, cond } = require('lodash');
+const _ = require('lodash');
 const { wireOne, wireTwo } = require('./aoc2019-3.data');
 
 // Part 1
 const getPath = (wire) => {
-  const coords = [];
+  const coords = {};
   let x = 0;
   let y = 0;
-  each(wire, (move) => {
-    const [direction, numMoves] = [head(move), toNumber(join(tail(move), ''))];
+  let numSteps = 0;
+  _.each(wire, (move) => {
+    const [direction, numMoves] = [_.head(move), _.toNumber(_.join(_.tail(move), ''))];
     for (let index = 0; index < numMoves; index += 1) {
-      coords.push([x, y]);
       if (direction === 'R') {
         x += 1;
       }
@@ -22,6 +22,8 @@ const getPath = (wire) => {
       if (direction === 'U') {
         y += 1;
       }
+      numSteps += 1;
+      _.set(coords, `${[x, y]}`, numSteps);
     }
   });
   return coords;
@@ -30,10 +32,19 @@ const getPath = (wire) => {
 const getClosestCrossingPoint = (wire1, wire2) => {
   const wireOnePath = getPath(wire1);
   const wireTwoPath = getPath(wire2);
+  const crossingPoints = _.pickBy(wireOnePath, (w1v, w1k) => _.has(wireTwoPath, w1k));
 
-  const crossingPoints = intersectionBy(wireOnePath, wireTwoPath, (x) => toString(x));
-  return min(map(tail(crossingPoints), ([x, y]) => sum([Math.abs(x), Math.abs(y)])));
+  // part 1 solution
+  const crossingPointSums = _(crossingPoints)
+    .keys()
+    .map(k => _.split(k, ','))
+    .map(([x, y]) => Math.abs(x) + Math.abs(y))
+    .value();
+  console.log(_.min(crossingPointSums));
+
+  // part 2 solution
+  const solution = _.min(_.map(crossingPoints, (v, k) => wireOnePath[k] + wireTwoPath[k]));
+  console.log(solution);
 };
 
-const solution = getClosestCrossingPoint(wireOne, wireTwo);
-console.log(solution);
+getClosestCrossingPoint(wireOne, wireTwo);
